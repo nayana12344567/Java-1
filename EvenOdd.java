3b2a5c04c44d0a9958c83d5eca054cfcf2611273
nayana12344567 <Nayana.23cs080@sode-edu.in>
2024-10-26 09:29:56 +0530

    Update

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenOddChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for input
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // Check if the number is even or odd
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+
+        scanner.close();
+   }}
