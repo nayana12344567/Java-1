727b7849afa14462df4e77a32f3cb5d13c4b05ce
nayana12344567 <Nayana.23cs080@sode-edu.in>
2024-11-14 10:58:02 +0530

    Add files via upload

---
+++ TestTools2.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1.pkg2;
+
+import pkg1.Tools;
+
+/**
+ *
+ * @author smv
+ */
+public class TestTools2 {
+    public static void main(String[] args){
+        Tools t1=new Tools();
+        t1.numbers(10);
+        t1.even(10);
+        t1.odd(10);
+        t1.mul3(10);
+        t1.mul4(10);
+        t1.mul5(10);
+       
+        
+        
+    }
+    
+}
+
+    
+
