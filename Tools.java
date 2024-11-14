5e38f9f67a8b472412ce7f063056a7073e5e2371
nayana12344567 <Nayana.23cs080@sode-edu.in>
2024-11-14 10:23:16 +0530

    Add files via upload

---
+++ Tools.java
@@ -0,0 +1,47 @@
+package pkg1;
+public class Tools {
+    public void numbers(int n1){
+        for(int i=0;i<=n1;i++){
+            System.out.print(i+" ");
+        
+        }
+        System.out.println(" ");
+    }
+         public void even(int n1){
+        for(int i=0;i<=n1;i++){
+            System.out.print(i*2+" ");
+        }
+         
+        System.out.println(" ");
+         }
+         
+          public void odd(int n1){
+          for(int i=0;i<=10;i++){
+            System.out.print(+i*2+1+" ");
+            }
+            System.out.println(" ");
+          }
+           public void mul3(int n1){
+            for(int i=0;i<=n1;i++){
+            System.out.print(+3*i+" ");
+            }
+            System.out.println(" ");
+           }
+            public void mul4(int n1){
+            for(int i=0;i<=n1;i++){
+            System.out.print(+4*i+" ");
+            }
+            System.out.println(" ");
+            }
+            public void mul5(int n1){
+            for(int i=0;i<=n1;i++){
+            System.out.print(+5*i+" ");
+            }
+             System.out.println(" ");
+            }
+            
+            
+            
+        }
+    
+
